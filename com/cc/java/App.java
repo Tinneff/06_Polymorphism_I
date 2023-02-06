25a3c4b02d558c9b76ee0028f1589d0a379ebb30
Peter <schmidt.multimedia@gmail.com>
2023-02-06 09:32:47 +0100

    Grundstruktur / doYourJob() / fly() in Superclass

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,6 +4,21 @@ public class App {
     
     public static void main(String[] args) {
      
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        output(queen.doYourJob());
+        output(worker.doYourJob());
+        output(drone.doYourJob());
+
+        output("--------------------");
+
+        output(queen.fly());
+        output(worker.fly());
+        output(drone.fly());
+
+
     }
 
     private static void output(String outputStr) {
